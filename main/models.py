b4d4c176db974a833920930f566aa8768128c7ad
Will S <williamsusanto82@gmail.com>
2023-10-21 15:03:17 +0700

    Login Logout Register

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from main.views import *
+from django.urls import path
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+]
