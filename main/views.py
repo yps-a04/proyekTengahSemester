c7dd92e364e606be6b17594b6d00babb23035de0
Daffa Zuhdii <daffaakmal2101@gmail.com>
2023-10-23 20:46:33 +0800

    add auth

--- main/views.py
+++ main/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.models import Book
 from django.core.paginator import Paginator
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm, SignUpForm
 # Create your views here.
 
 
@@ -16,3 +20,42 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
+
+def register(request):
+    form = SignUpForm()
+    context = {'form': form}
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Your account has been successfully created!')
+            return redirect('main:login')
+        else:
+            messages.info(
+                request, "Register Failed. Make sure your password is strong.")
+
+    return render(request, 'register.html', context)
+
+
+def login_user(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(
+                request, 'Sorry, incorrect username or password. Please try again.')
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('main:login')
