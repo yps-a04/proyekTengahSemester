69f0007cd3ef166d0db79a2101cad60034841d7e
Muhammad Iqbal <miqbalsparta2004@gmail.com>
2023-10-28 14:54:46 +0700

    restrict user access

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from main.models import Book
 from django.core.paginator import Paginator
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.urls import reverse
 from bookmark.models import Bookmark
 from django.contrib.auth.models import User
@@ -9,19 +9,21 @@ from django.shortcuts import get_object_or_404
 
 
 def show_bookmark(request, key):
-    username = key
-    user = User.objects.get(username=username)
-    book = user.bookmarked.select_related('book')
+    if request.user.username == key:  
+        username = key
+        user = User.objects.get(username=username)
+        book = user.bookmarked.select_related('book')
 
-    p = Paginator(book, 20)
-    page = request.GET.get('page')
+        p = Paginator(book, 20)
+        page = request.GET.get('page')
 
-    books = p.get_page(page)
-    context = {
-        'books': books,
-        'username' : username,
-    }
-    return render(request, "bookmark.html", context)
+        books = p.get_page(page)
+        context = {
+            'books': books,
+            'username' : username,
+        }
+        return render(request, "bookmark.html", context)
+    return HttpResponse("cannot access")
 
 def delete_bookmark(request, key):
     book = get_object_or_404(Book, pk=key)
