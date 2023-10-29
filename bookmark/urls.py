6aa532f6ab688ffe8778fe5b6d520c636d8e6bbd
Muhammad Iqbal <miqbalsparta2004@gmail.com>
2023-10-29 21:23:37 +0700

    gagal sort bookmark

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from bookmark.views import show_bookmark, delete_bookmark
+from bookmark.views import show_bookmark, delete_bookmark, sort_bookmark
 
 app_name = 'bookmark'
 
 urlpatterns = [
     path('<str:key>/', show_bookmark, name='show_bookmark'),
-    path('delete/<int:key>', delete_bookmark, name="delete_bookmark")
+    path('delete/<int:key>', delete_bookmark, name="delete_bookmark"),
 ]
 
